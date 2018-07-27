344e0798bb7d5c1c445b6bf25f103f90c7b4d799
Marcelo <moser28064212@gmail.com>
2018-07-27 19:44:29 -0300

    Added postcss-import

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 var postcss = require('gulp-postcss');
+var cssimport = require('postcss-import');
 var cssvar = require('postcss-simple-vars');
 var cssnested = require('postcss-nested');
 var autoprefixer = require('autoprefixer');
@@ -10,7 +11,7 @@ var autoprefixer = require('autoprefixer');
 //TASKS
 gulp.task('css', function() {
 	return gulp.src('./app/assets/styles/**/*.css') 
-		.pipe(postcss([cssvar, autoprefixer({browsers: ['cover 99.5%']}), cssnested]))
+		.pipe(postcss([cssimport, cssvar, autoprefixer({browsers: ['cover 99.5%']}), cssnested]))
 		.pipe(gulp.dest('./app/assets/output/css'))
 		.pipe(browserSync.stream());
 });
@@ -18,7 +19,7 @@ gulp.task('css', function() {
 gulp.task('browser-sync', ['css'], function() {
 	browserSync.init({ server:"app" });
 
-	gulp.watch('app/assets/styles/*.css', ['css']);
+	gulp.watch('app/assets/styles/**/*.css', ['css']);
 	gulp.watch('app/*.html').on('change', browserSync.reload);
 });
 
