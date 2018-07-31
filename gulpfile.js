60eb27c2d1c1892c71c7c3cc1d0d7da88cd1e26a
Marcelo <moser28064212@gmail.com>
2018-07-31 16:31:42 -0300

    Added gulp-plumber to prevent break gulp with errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var cssimport = require('postcss-import');
 var cssvar = require('postcss-variables');
@@ -9,13 +10,20 @@ var cssnested = require('postcss-nested');
 var autoprefixer = require('autoprefixer');
 
 //TASKS
+
+//POSTCSS
 gulp.task('css', function() {
 	return gulp.src('./app/assets/styles/**/*.css') 
+		.pipe(plumber(function(err) {
+			console.log('err');
+			this.emit('end');
+		}))
 		.pipe(postcss([cssimport, cssvar, autoprefixer({browsers: ['cover 99.5%']}), cssnested]))
 		.pipe(gulp.dest('./app/assets/output/css'))
 		.pipe(browserSync.stream());
 });
 
+//BROWSER-SYNC
 gulp.task('browser-sync', ['css'], function() {
 	browserSync.init({ server:"app" });
 
@@ -23,4 +31,5 @@ gulp.task('browser-sync', ['css'], function() {
 	gulp.watch('app/*.html').on('change', browserSync.reload);
 });
 
+//DEFAULT
 gulp.task('default', ['browser-sync']);
