e1916dc01311d604985b9c09c879c930c164d045
moser89 <moser28064212@gmail.com>
2018-08-16 11:09:08 -0300

    Added files

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+ //REQUIRES
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var cssimport = require('postcss-import');
+var cssvar = require('postcss-variables');
+var cssnested = require('postcss-nested');
+var autoprefixer = require('autoprefixer');
+var mixins = require('postcss-mixins');
+
+//TASKS
+
+//POSTCSS
+gulp.task('css', function() {
+	return gulp.src('./app/assets/styles/**/*.css') 
+		.pipe(plumber(function(err) {
+			console.log('err');
+			this.emit('end');
+		}))
+		.pipe(postcss([cssimport, mixins, cssvar, autoprefixer({browsers: ['cover 99.5%']}), cssnested]))
+		.pipe(gulp.dest('./app/assets/output/css'))
+		.pipe(browserSync.stream());
+});
+
+//BROWSER-SYNC
+gulp.task('browser-sync', ['css'], function() {
+	browserSync.init({ server:"app" });
+
+	gulp.watch('app/assets/styles/**/*.css', ['css']);
+	gulp.watch('app/*.html').on('change', browserSync.reload);
+});
+
+//DEFAULT
+gulp.task('default', ['browser-sync']);
